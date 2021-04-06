0bc70f91cc8470160b557541647889bf4b4b500e
Brian Oxley <boxley@thoughtworks.com>
2021-04-06 11:55:38 +0800

    Use toggles

--- src/main/kotlin/hm/binkley/labs/Foo.kt
+++ src/main/kotlin/hm/binkley/labs/Foo.kt
@@ -1,12 +1,24 @@
 package hm.binkley.labs
 
+import hm.binkley.labs.Foo.Cool
 import java.util.Properties
 
 val FEATURE_A = isEnabled("featureA");
 val FEATURE_B = isEnabled("featureB");
-val FEATURE_C = isEnabled("featureC");
 
-class Foo
+class Foo {
+    fun interface Cool {
+        fun nifty(): String
+    }
+
+    private val cool = when {
+        FEATURE_A -> Cool { "Apple" }
+        FEATURE_B -> Cool { "Banana" }
+        else -> throw IllegalArgumentException("Bad feature toggle")
+    }
+
+    fun nifty() = cool.nifty()
+}
 
 // TODO: Does stdlib provide help here?
 private fun isEnabled(name: String) = Properties().apply {
--- src/test/kotlin/hm/binkley/labs/FooTest.kt
+++ src/test/kotlin/hm/binkley/labs/FooTest.kt
@@ -1,25 +1,20 @@
 package hm.binkley.labs.hm.binkley.labs
 
-import hm.binkley.labs.FEATURE_A
-import hm.binkley.labs.FEATURE_B
-import hm.binkley.labs.FEATURE_C
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import hm.binkley.labs.Foo
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Test
 
 internal class FooTest {
+    private val foo = Foo()
+
     @Test
     fun `should have feature A`() {
-        assertTrue(FEATURE_A)
+        assertEquals("Apple", foo.nifty())
     }
 
     @Test
     fun `should not have feature B`() {
-        assertFalse(FEATURE_B)
-    }
-
-    @Test
-    fun `should not have feature C`() {
-        assertFalse(FEATURE_C)
+        assertNotEquals("Banana", foo.nifty())
     }
 }
