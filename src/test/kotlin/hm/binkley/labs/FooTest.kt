ef15ab316931625de940d972c2a6be5f8868e4af
Brian Oxley <boxley@thoughtworks.com>
2021-04-06 16:26:02 +0800

    Readability

--- src/test/kotlin/hm/binkley/labs/FooTest.kt
+++ src/test/kotlin/hm/binkley/labs/FooTest.kt
@@ -3,13 +3,13 @@ package hm.binkley.labs
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-private var toggles = System.getProperty("toggles", "").split(",")
+private var toggles = System.getProperty("toggles", "extraCool").split(",")
 
 internal class FooTest {
     @Test
-    fun `should not use disabled feature toggle`() {
+    fun `should use extraCool feature`() {
         val fooWithExtraCool = Foo(toggles)
 
-        assertEquals("Banana", fooWithExtraCool.nifty())
+        assertEquals("Apple", fooWithExtraCool.nifty())
     }
 }
