835940e70133b7dca066dcde0dd2aaed109362f9
Brian Oxley <boxley@thoughtworks.com>
2021-04-06 12:25:28 +0800

    Simpler

--- src/main/kotlin/hm/binkley/labs/Foo.kt
+++ src/main/kotlin/hm/binkley/labs/Foo.kt
@@ -1,16 +1,9 @@
 package hm.binkley.labs
 
-import lombok.Generated
-import java.util.Properties
-
-val FEATURE_A = isEnabled("featureA");
-val FEATURE_B = isEnabled("featureB");
-
-class Foo {
-    private val cool = when {
-        FEATURE_A -> coolA()
-        FEATURE_B -> coolB()
-        else -> throw IllegalArgumentException("Bad feature toggle")
+class Foo(featureA: String) {
+    private val cool = when (featureA) {
+        "A" -> coolA()
+        else -> coolB()
     }
 
     fun nifty() = cool.nifty()
@@ -20,13 +13,6 @@ private fun interface Cool {
     fun nifty(): String
 }
 
-@Generated
 private fun coolA() = Cool { "Apple" }
 
-@Generated
 private fun coolB() = Cool { "Banana" }
-
-// TODO: Does stdlib provide help here?
-private fun isEnabled(name: String) = Properties().apply {
-    load(Foo::class.java.getResourceAsStream("/foo.properties"))
-}.getProperty(name).toBoolean()
--- src/test/kotlin/hm/binkley/labs/FooTest.kt
+++ src/test/kotlin/hm/binkley/labs/FooTest.kt
@@ -2,19 +2,16 @@ package hm.binkley.labs.hm.binkley.labs
 
 import hm.binkley.labs.Foo
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Test
 
 internal class FooTest {
-    private val foo = Foo()
-
     @Test
     fun `should have feature A`() {
-        assertEquals("Apple", foo.nifty())
+        assertEquals("Apple", Foo("A").nifty())
     }
 
     @Test
     fun `should not have feature B`() {
-        assertNotEquals("Banana", foo.nifty())
+        assertEquals("Banana", Foo("B").nifty())
     }
 }
