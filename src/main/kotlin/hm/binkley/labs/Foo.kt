0af80579b8de305065d2eb02a67957004b99ef26
Brian Oxley <boxley@thoughtworks.com>
2021-04-06 12:07:13 +0800

    Experiment

--- src/main/kotlin/hm/binkley/labs/Foo.kt
+++ src/main/kotlin/hm/binkley/labs/Foo.kt
@@ -1,24 +1,30 @@
 package hm.binkley.labs
 
-import hm.binkley.labs.Foo.Cool
+import lombok.Generated
 import java.util.Properties
 
 val FEATURE_A = isEnabled("featureA");
 val FEATURE_B = isEnabled("featureB");
 
 class Foo {
-    fun interface Cool {
-        fun nifty(): String
-    }
-
     private val cool = when {
-        FEATURE_A -> Cool { "Apple" }
-        FEATURE_B -> Cool { "Banana" }
+        FEATURE_A -> coolA()
+        FEATURE_B -> coolB()
         else -> throw IllegalArgumentException("Bad feature toggle")
     }
 
     fun nifty() = cool.nifty()
 }
+
+private fun interface Cool {
+    fun nifty(): String
+}
+
+@Generated
+private fun coolA() = Cool { "Apple" }
+
+@Generated
+private fun coolB() = Cool { "Banana" }
 
 // TODO: Does stdlib provide help here?
 private fun isEnabled(name: String) = Properties().apply {
